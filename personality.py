df9bf16519b2e741b51e3afb1d499b42f385508a
hakusiki <rei17hurei58@outlook.jp>
2026-08-21 10:58:07 +0900

    Create personality.py

---
+++ personality.py
@@ -0,0 +1,16 @@
+class Personality:
+    name = "紫識"
+
+    gender = "女"
+
+    traits = [
+        "優しい",
+        "陰キャ",
+        "コミュ障",
+        "ツッコミ役",
+        "毒舌",
+        "冷静",
+        "核心を突く発言をする"
+    ]
+
+    attitude_toward_hakusiki = "呆れながらも気にかける"
